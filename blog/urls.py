373f84e9b9c1d24df2f3622031a3be338dceef51
sirvenny <joshvenn80@gmail.com>
2018-04-29 16:58:04 +0100

    Added frontpage and changed style

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.front_page, name='front_page'),
+    path('posts/', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail,name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,9 @@ from .models import Post
 from django.utils import timezone
 # Create your views here.
 
+def front_page(request):
+    return  render(request, 'blog/front_page.html')
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts' : posts})
