6c4c5a626c3bce628a902109341c2a93a74f545d
sirvenny <joshvenn80@gmail.com>
2018-06-03 14:14:15 +0100

    Added front page latest details on cards, post list set as media class so it is responsive on smaller resolutions, post detail now has post picture in center, about pages added with cards for links

--- blog/static/js/rango-jquery.js
+++ blog/static/js/rango-jquery.js
@@ -1,11 +1,11 @@
 $(window).scroll(function() {
      if ($(document).scrollTop() > 100) {
        $('nav').addClass('shrink');
-       $(".fade-text").fadeOut(1500);
+       $(".fade-text").fadeOut(1000);
        $(".head-letters").addClass('shrink');
      } else {
        $('nav').removeClass('shrink');
-       $('.fade-text').fadeIn(1500);
+       $('.fade-text').fadeIn(1000);
        $(".head-letters").removeClass('shrink')
      }
 });
--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.front_page, name='front_page'),
     path('posts/', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail,name='post_detail'),
+    path('about/', views.about, name='about'),
 ]
