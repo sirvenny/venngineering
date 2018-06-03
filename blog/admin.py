6ef3b6bc9bf25ec45a351fdd45c99173b08cbcd7
sirvenny <joshvenn80@gmail.com>
2018-06-03 14:52:33 +0100

    remove markdown

--- blog/admin.py
+++ blog/admin.py
@@ -1,18 +1,5 @@
 from django.contrib import admin
 from .models import Post
 from django.db import models
-from markdownx.widgets import AdminMarkdownxWidget
-from markdownx.admin import MarkdownxModelAdmin
-
-# Register your models here.
-
-# class PostAdmin(admin.ModelAdmin):
-#     fields = ['author', 'title', 'text', 'created_date', 'published_date', 'tags']
-#     formfield_overrides = {
-#     models.TextField: {'widget': AdminMarkdownxWidget}
-#     }
-    # list_display = ('title', 'published_date')
-
 
 admin.site.register(Post)
-# admin.site.register(Post, PostAdmin)
